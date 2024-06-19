3c51a8bdf46a16864a9469e067deff69c16809d2
artofcoding-by-hanif <hanifsma2@gmail.com>
2024-06-19 12:04:30 +0530

    code clean

--- src/com/artofcoding/factory/ViewEngine.java
+++ src/com/artofcoding/factory/ViewEngine.java
@@ -3,5 +3,5 @@ package src.com.artofcoding.factory;
 import java.util.Map;
 
 public interface ViewEngine {
-    public String render(String viewName, Map<String, Object> context);
+    String render(String viewName, Map<String, Object> context);
 }
